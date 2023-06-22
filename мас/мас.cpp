101648ce1e757a4361b6adb7cbace5b595ff4ef4
Ajlik <bbgf05436@gmail.com>
2023-06-22 22:31:17 +0300

    Добавьте файлы проекта.

---
+++ мас/мас.cpp
@@ -0,0 +1,33 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+	srand(time(NULL));
+	setlocale(LC_ALL, "RU");
+
+	float mass[1000] = {2,4,7,2,6};
+	float summa = 0;
+	float min = mass[0];
+	
+	
+	for (int i = 0; i < 10; i++) {
+		cout << "введите эллемент:" << i << ":" << endl;
+		cin >> mass[i];
+	}
+	
+	for (int i = 0; i < 10; i++) {
+		cout << "эллемент:" << i << ": " << mass[i] << endl;
+	}
+	
+	for (int i = 0; i < 10; i++) {
+		summa += mass[i];
+		if (mass[i] < min)
+			min = mass[i];
+	
+	}
+	cout << "сумма: " << summa<< endl;
+	cout << "минимальное число: " << min;
+	return 0;
+}
+
